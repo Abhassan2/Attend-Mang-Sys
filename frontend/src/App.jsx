fa2b75922d8a403707e4cd73d5800d292f0ca125
Abid Hassan <abidhassan2073@gmail.com>
2026-03-14 10:39:00 +0530

    css changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import TimeTable from '../Pages/TimeTable.jsx';
 import Attendance from '../Pages/Attendance.jsx';
+import Courses from '../Pages/Courses.jsx';
+
 function App(){
 
   return(
     // <TimeTable/>
     <Attendance />
+    // <Courses />
   )
 }
 
