bae3ef372d2c49256f73e70f874541b8efc2cbe3
Abid Hassan <abidhassan2073@gmail.com>
2026-03-16 21:17:31 +0530

    added Header.jsx file

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+        <p>menu icon</p>
+        <p>profile icon/brand</p>
+    </div>
+  )
+}
