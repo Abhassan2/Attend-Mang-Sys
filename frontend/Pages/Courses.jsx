ec2aa92b6a60f05edceba3ea293e8e64ef9866cf
Abid Hassan <abidhassan2073@gmail.com>
2026-03-17 08:19:45 +0530

    design the Course.jsx component

--- frontend/Pages/Courses.jsx
+++ frontend/Pages/Courses.jsx
@@ -4,6 +4,9 @@ export default function Courses() {
   return (
     <div className="container">
       <AttendanceOverview overview={{msg: "hello", totalPercent: 82}} />
+      <div className="card-container subContainer">
+        <h1>My Courses</h1>
+      </div>
     </div>
   )
 }
