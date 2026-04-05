3a818a7da49da9117bed0482f52abe7218f545fe
Abid Hassan <abidhassan2073@gmail.com>
2026-04-05 18:43:09 +0530

    added icons in sidebar

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -2,6 +2,10 @@ import '../Style/Sidebar.css';
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AttendEaseContext } from "../Context/AttendEaseContext";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import LocalLibraryRoundedIcon from '@mui/icons-material/LocalLibraryRounded';
+import ViewTimelineRoundedIcon from '@mui/icons-material/ViewTimelineRounded';
+import LeaderboardRoundedIcon from '@mui/icons-material/LeaderboardRounded';
 
 export default function Sidebar() {
   const { object } = useContext(AttendEaseContext);
@@ -16,17 +20,21 @@ export default function Sidebar() {
       {object.openSidebar && (
         <div className="sidebar">
           <ul>
-            <NavLink to="/home" className="primary-text" onClick={(e)=> handleActiveTab(e)}>
-              <li>Home</li>
+            <NavLink to="/home" className="primary-text flex " onClick={(e)=> handleActiveTab(e)}>
+              <HomeRoundedIcon />
+              <li className='mx-2'>Home</li>
             </NavLink>
-            <NavLink to="/attendance" className="primary-text" onClick={(e)=> handleActiveTab(e)}>
-              <li>Attendance</li>
+            <NavLink to="/attendance" className="primary-text flex" onClick={(e)=> handleActiveTab(e)}>
+              <LeaderboardRoundedIcon />
+              <li className='mx-2'>Attendance</li>
             </NavLink>
-            <NavLink to="/time-table" className="primary-text" onClick={(e)=> handleActiveTab(e)}>
-              <li>Time-Table</li>
+            <NavLink to="/time-table" className="primary-text flex" onClick={(e)=> handleActiveTab(e)}>
+              <ViewTimelineRoundedIcon />
+              <li className='mx-2'>Time-Table</li>
             </NavLink>
-            <NavLink to="/courses" className="primary-text" onClick={(e)=> handleActiveTab(e)}>
-              <li>Courses</li>
+            <NavLink to="/courses" className="primary-text flex" onClick={(e)=> handleActiveTab(e)}>
+              <LocalLibraryRoundedIcon />
+              <li className='mx-2'>Courses</li>
             </NavLink>
           </ul>
         </div>
